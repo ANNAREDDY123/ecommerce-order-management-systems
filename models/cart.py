6a50afbff063089424ca7f9214bad0d98ff47184
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-29 15:58:17 +0530

    Create cart.py

---
+++ models/cart.py
@@ -0,0 +1,28 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    ForeignKey
+)
+
+from database import Base
+
+
+class Cart(Base):
+    __tablename__ = "cart"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    user_id = Column(
+        Integer,
+        ForeignKey("users.id")
+    )
+
+    product_id = Column(
+        Integer,
+        ForeignKey("products.id")
+    )
+
+    quantity = Column(Integer)
