2e2e0e4bcbd9c693d00a1dc9838dafdb7bf9bc88
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-29 15:59:19 +0530

    Create payment.py

---
+++ models/payment.py
@@ -0,0 +1,32 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    Float,
+    String,
+    ForeignKey
+)
+
+from database import Base
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    order_id = Column(
+        Integer,
+        ForeignKey("orders.id")
+    )
+
+    amount = Column(Float)
+
+    payment_method = Column(String)
+
+    payment_status = Column(
+        String,
+        default="Pending"
+    )
